901135c02e7a56e10efe2e48396025bfa9cbb71a
gayanega <gabrielmark.gayanelo@student.shu.edu>
2018-05-01 14:29:26 -0400

    pls

--- tests/event.spec.js
+++ tests/event.spec.js
@@ -12,6 +12,9 @@ describe ('Event module', () => {
         })
     })
 
+
+
+
     describe('"getEvents"', () => {
         it ('should have a getEvents method', () => {
             expect(event.getEvents).to.be.a('function');
@@ -22,6 +25,9 @@ describe ('Event module', () => {
         })
     })
 
+
+
+
     describe('"getEventById"', () => {
         it ('should have a getEventById method', () => {
             expect(event.getEventById).to.be.a('function');
@@ -31,4 +37,56 @@ describe ('Event module', () => {
             expect(event.getEventById(1)).to.be.an('object');
         })
     })
+
+
+
+    describe('"getEventName', () => {
+        it('should have a getEventName method', () => {
+            expect(event.getEventName).to.be.a('function');
+        })
+
+        it('should return array of event names', () => {
+            expect(event.getEventName(1)).to.be.an('object');
+        })
+    })
+
+
+
+    describe('"getEventRegistrants', () => {
+        it('should have a getEventRegistrants method', () => {
+            expect(event.getEventRegistrants).to.be.a('function');
+        })
+
+        it('should return array of event registrant names', () => {
+            expect(event.getEventRegistrants(1)).to.be.an('object');
+        })
+    })
+
+
+
+    describe('"getEventDescription', () => {
+        it('should have a getEventDescription method', () => {
+            expect(event.getEventDescription).to.be.a('function');
+        })
+
+        it('should return array of event registrants', () => {
+            expect(event.getEventDescription()).to.be.an('string');
+        })
+    })
+
+
+
+    describe('"getEventDateTime', () => {
+        it('should have a getEventDateTime method', () => {
+            expect(event.getEventDateTime).to.be.a('function');
+        })
+
+        it('should return event date and time', () => {
+            expect(event.getEventDateTime()).to.be.an('string');
+        })
+    })
+
+
+
+
 })
