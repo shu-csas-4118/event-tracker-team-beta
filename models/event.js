774ba3b1e384fae758fc6f084d1ce844d742b90b
gayanega <gabrielmark.gayanelo@student.shu.edu>
2018-05-01 23:44:55 -0400

    Added addRegistrant()

--- models/event.js
+++ models/event.js
@@ -1,6 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var eventSchema = new Schema({
+    date: String,
+    time: String,
+    Name: String,
+    Description: String,
+    Id: Number,
+    registrant: Array
+
+});
+
+eventSchema.methods.addRegistrant() = function(account){
+    if(this.registrant.has(account))
+    return "Registrant already added.";
+    else {this.registrants.push(account);
+        return "Registrant Added."
+    }
+}
 
 function Event(){
 };
+
+
 
 Event.prototype.getEvents = function () {
     return [];
