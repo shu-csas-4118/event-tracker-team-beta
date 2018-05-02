8e167622fe72cb83bba76f0db64cebab9edfa226
Zohar Suslovich <zohar.suslovich@shu.edu>
2018-05-02 02:25:41 -0400

    fixed redirecting to register page

---
+++ routes/register.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET register page. */
+router.get('/', function(req, res, next) {
+    res.render('register', { title: 'Register' });
+});
+
+
+
+
+module.exports = router;
