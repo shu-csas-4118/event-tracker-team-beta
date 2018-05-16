cd01c556773ac0ec4b41cd8c5c4abf6174a87c3a
Matt Kim <yong.kim@student.shu.edu>
2018-05-16 01:34:29 -0400

    added Home Page. edited method in routes to test it for now

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/about', function(req, res, next) {
 });
 
 router.get('/userRegister', function(req, res, next) {
-  res.render('userRegister', {});
+  res.render('home', {});
 });
 
 
